17665becad3032b99d258b7336b1708b438993ec
Ryan <ryan@artcommaco.de>
2019-01-29 09:12:37 +0000

    rewrite `fetchMixes` to fetch mixes in order

--- src/components/App.js
+++ src/components/App.js
@@ -29,26 +29,33 @@ class App extends Component {
   fetchMixes = async () => {
     const {mixIds} = this.state;
 
-    // here we loop over our mix ids and fetch each other
-    mixIds.map(async id => {
+    // we `reduce` over the mixes to make sure they're always returned in order,
+    // if we used `map` here instead whichever mix came back from the api first
+    // would be the one set first in the state
+    const mixes = await mixIds.reduce(async (asyncMixes, id) => {
       try {
-        // always remember await when using fetch in an async function
-        const response = await fetch(
-          // we add the id onto the end of our url as a dynamic segment
-          `https://api.mixcloud.com${id}`
-        );
+        // always remember to `await` when using fetch in an async function
+        // we add the id onto the end of our url as a dynamic segment
+        const response = await fetch(`https://api.mixcloud.com${id}`);
         const data = await response.json();
-
-        // put the mix into our state
-        this.setState((prevState, props) => ({
-          // here we add our data onto the end of all of
-          // our previous state using the spread
-          mixes: [...prevState.mixes, data]
-        }));
+        // because this callback inside our `reduce` is an async function we need
+        // to get our `mixes` back out of the promise before we can add the new mix,
+        // we can do this by `await`ing them
+        const mixes = await asyncMixes;
+        // now we can add our `data` onto the end of all of the rest of the `mixes`
+        // with an array spread
+        return [...mixes, data];
       } catch (error) {
         console.log(error);
+        return mixes;
       }
-    });
+      // the initial value of `mixes` is an empty array wrapped in a promise.
+      // this means that line 44 will work the first time through the `mixIds`
+      // as well as every other time
+    }, Promise.resolve([]));
+
+    // and finally set the `mixes` to our state
+    this.setState({mixes});
   };
 
   mountAudio = async () => {
@@ -121,15 +128,27 @@ class App extends Component {
           {/* this div contians our page (excluding audio player) */}
           <div className="flex-l justify-end">
             {/* FeaturedMix (needs styling and updating) */}
-            <FeaturedMix {...this.state} {...this.actions} {...firstMix} id={firstMix.key} />
+            <FeaturedMix
+              {...this.state}
+              {...this.actions}
+              {...firstMix}
+              id={firstMix.key}
+            />
             <div className="w-50-l relative z-1">
               {/* Header (needs styling and updating)  */}
               <Header />
               {/* Routed page */}
               {/* here we pass our state and our actions down into the
               home component so that we can use them */}
-              <Route exact path="/" render={() => <Home {...this.state} {...this.actions} />} />
-              <Route path="/archive" render={() => <Archive {...this.state} {...this.actions} />} />
+              <Route
+                exact
+                path="/"
+                render={() => <Home {...this.state} {...this.actions} />}
+              />
+              <Route
+                path="/archive"
+                render={() => <Archive {...this.state} {...this.actions} />}
+              />
               <Route path="/about" render={() => <About {...this.state} />} />
             </div>
           </div>
